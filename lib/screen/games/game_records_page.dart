3488ca008353038ff414fe419cc2eb675d2a885a
LahiruLakshan <balahirulakshan@gmail.com>
2024-05-20 11:01:03 +0530

    added snake game timer

---
+++ lib/screen/games/game_records_page.dart
@@ -0,0 +1,170 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../theme/app_theme.dart';
+import '../loading_screen.dart';
+
+class GameRecordsPage extends StatefulWidget {
+  final DocumentSnapshot<Object?>? currentUser;
+
+  const GameRecordsPage({Key? key,  this.currentUser}) : super(key: key);
+
+  @override
+  State<GameRecordsPage> createState() => _GameRecordsPageState();
+}
+
+class _GameRecordsPageState extends State<GameRecordsPage> {
+  final _pageCtrl = PageController(viewportFraction: 0.9);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenSize = MediaQuery.of(context).size.height;
+    double height = screenSize - kToolbarHeight - 90.0;
+
+    final Stream<QuerySnapshot> _recordsStream = FirebaseFirestore.instance
+        .collection('game')
+        .snapshots();
+
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      appBar: AppBar(
+        title: Text("Game Records", style: TextStyle(color: AppTheme.colors.blue),),
+        backgroundColor: AppTheme.colors.white,
+        iconTheme: IconThemeData(
+          color: AppTheme.colors.blue, //change your color here
+        ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background.png"),
+            opacity: 0.8,
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: _recordsStream,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return LoadingScreen();
+            }
+            if (snapshot.hasError) {
+              return Center(
+                child: Text("some error occured",
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              );
+            }
+            if (snapshot.hasData) {
+              final List<DocumentSnapshot> recordData = snapshot.data!.docs;
+
+
+              return Stack(
+                children: [
+                  ListView.builder(
+                      controller: _pageCtrl,
+                      itemCount: recordData?.length,
+                      itemBuilder: (context, index) {
+                        // var travel = _list[index];
+                        final tumorRecord = recordData![index];
+                        if(recordData.isNotEmpty){
+                          return GestureDetector(
+                            onTap: () {
+
+                            },
+                            child: Stack(
+                              children: <Widget>[
+                                Container(
+                                  margin: EdgeInsets.fromLTRB(40.0, 5.0, 20.0, 5.0),
+                                  height: 170.0,
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    color: AppTheme.colors.blue,
+                                    borderRadius: BorderRadius.circular(20.0),
+                                  ),
+                                  child: Padding(
+                                    padding:
+                                    EdgeInsets.fromLTRB(100.0, 20.0, 20.0, 20.0),
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Container(
+                                          // width: 120.0,
+                                          child: Flexible(
+                                            child: Text(
+                                              "brikeBrakerSec : ${tumorRecord["brikeBrakerSec"]}",
+                                              style: TextStyle(
+                                                fontSize: 14.0,
+                                                color: AppTheme.colors.white,
+                                                fontWeight: FontWeight.w600,
+                                              ),
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 2,
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(height: 10.0),
+                                        Container(
+                                          // width: 120.0,
+                                          child: Flexible(
+                                            child: Text(
+                                              "flappyBirdsec : ${tumorRecord["flappyBirdsec"]}",
+                                              style: TextStyle(
+                                                fontSize: 14.0,
+                                                color: AppTheme.colors.white,
+                                                fontWeight: FontWeight.w600,
+                                              ),
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 2,
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(height: 10.0),
+                                        Container(
+                                          // width: 120.0,
+                                          child: Flexible(
+                                            child: Text(
+                                              "snakeGamesec : ${tumorRecord["snakeGamesec"]}",
+                                              style: TextStyle(
+                                                fontSize: 14.0,
+                                                color: AppTheme.colors.white,
+                                                fontWeight: FontWeight.w600,
+                                              ),
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 2,
+                                            ),
+                                          ),
+                                        ),
+                                        SizedBox(height: 10.0),
+
+
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        } else{
+                          return Center(
+                            child: Text(
+                                "Empty!"
+                            ),
+                          );
+                        }
+
+                      }),
+                ],
+              );
+            }
+            return LoadingScreen();
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/screen/games/games_home.dart
+++ lib/screen/games/games_home.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:cerebral_snap/screen/games/game_records_page.dart';
 import 'package:cerebral_snap/screen/games/snake_game_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:device_apps/device_apps.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 import '../../theme/app_theme.dart';
 import '../../widgets/app_elevated_button_icon.dart';
@@ -23,10 +27,54 @@ class _GamesHomeState extends State<GamesHome> {
   dynamic responseList;
   bool? isLoading = false;
   bool? isPredictionFinish = false;
+  final int classes = 5;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  String brikeBrakerSec = "Loading...";
+  String flappyBirdsec = "Loading...";
+  String snakeGamesec = "Loading...";
+  String status = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchDocument();
+  }
+
+  void _fetchDocument() async {
+
+      final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+      DocumentReference documentReference = _firestore.collection('temp').doc('temp_file');
+
+      DocumentSnapshot documentSnapshot = await documentReference.get();
+
+        setState(() {
+          brikeBrakerSec = documentSnapshot["brikeBrakerSec"].toString();
+          flappyBirdsec = documentSnapshot["flappyBirdsec"].toString();
+          snakeGamesec = documentSnapshot["snakeGamesec"].toString();
+        });
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
+    final width = MediaQuery.of(context).size.width;
+
+
+
+    Future<DocumentSnapshot<Object?>> _getDocument() async {
+      final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+      DocumentReference documentReference = _firestore.collection('temp').doc('temp_file');
+
+        DocumentSnapshot documentSnapshot = await documentReference.get();
+        setState(() {
+          brikeBrakerSec = documentSnapshot["brikeBrakerSec"].toString();
+          flappyBirdsec = documentSnapshot["flappyBirdsec"].toString();
+          snakeGamesec = documentSnapshot["snakeGamesec"].toString();
+        });
+        return documentSnapshot;
+    }
+
+    Future<DocumentSnapshot<Object?>> docData = _getDocument();
 
     return Scaffold(
       appBar: AppBar(
@@ -98,11 +146,11 @@ class _GamesHomeState extends State<GamesHome> {
                 const SizedBox(height: 30.0),
                 AppElevatedButtonIcon(
                   onPressed: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //       builder: (context) => const SnakeGamePage()),
-                    // );
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => GameRecordsPage(currentUser: widget.currentUser)),
+                    );
+
                   },
                   title: "Records",
                   fontSize: 18.0,
@@ -115,7 +163,74 @@ class _GamesHomeState extends State<GamesHome> {
                 ),
                 const SizedBox(height: 30.0),
 
+                Center(
+                  child: Text(
+                    "brikeBrakerSec : ${brikeBrakerSec}s",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: AppTheme.colors.blue, // Dark blue
+                      fontFamily: 'Raleway ', // Use a different font
+                    ),
+                  ),
+                ),Center(
+                  child: Text(
+                    "flappyBirdsec : ${flappyBirdsec}s",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: AppTheme.colors.blue, // Dark blue
+                      fontFamily: 'Raleway ', // Use a different font
+                    ),
+                  ),
+                ),Center(
+                  child: Text(
+                    "snakeGamesec : ${snakeGamesec}s",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: AppTheme.colors.blue, // Dark blue
+                      fontFamily: 'Raleway ', // Use a different font
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30.0),
+                AppElevatedButtonIcon(
+                  onPressed: () {
+                    String value = handlePredct(flappyBirdsec, snakeGamesec, brikeBrakerSec);
+                    setState(() {
+                      status = value.toString();
+                    });
 
+
+
+                  },
+                  title: "Predict Now",
+                  fontSize: 18.0,
+                  icon:
+                  const FaIcon(FontAwesomeIcons.arrowDown, size: 20),
+                  primary: AppTheme.colors.blue,
+                  onPrimary: AppTheme.colors.white,
+                  width: 200,
+                  borderRadius: 10,
+                ),
+                if (isLoading!)
+                  LoadingAnimationWidget.prograssiveDots(
+                    color: AppTheme.colors.blue,
+                    size: 100,
+                  ),
+                if(isLoading! == false && status != "")
+                  Center(
+                    child: Text(
+                      "Status : ${status}",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: AppTheme.colors.blue, // Dark blue
+                        fontFamily: 'Raleway ', // Use a different font
+                      ),
+                    ),
+                  ),
               ],
             ),
           ),
@@ -124,27 +239,42 @@ class _GamesHomeState extends State<GamesHome> {
     );
   }
 
-  // void handleUploadImage(File uploadImage) async {
-  //   final request = http.MultipartRequest("POST", Uri.parse(uri));
-  //   final headers = {"Content-type": "multipart/form-data"};
-  //
-  //   request.files.add(http.MultipartFile(
-  //       'audio',
-  //       uploadImage.readAsBytes().asStream(),
-  //       uploadImage.lengthSync(),
-  //       filename: uploadImage.path.split("/").last
-  //   ));
-  //
-  //   request.headers.addAll(headers);
-  //   final response = await request.send();
-  //   http.Response res = await http.Response.fromStream(response);
-  //   final resJson = jsonDecode(res.body);
-  //   print("Response => $resJson");
-  //   setState(() {
-  //     responseList = resJson;
-  //     isLoading = false;
-  //     isPredictionFinish = true;
-  //   });
-  //   print("prediction => ${responseList["prediction"]}");
-  // }
+
+  void handlePredict(File uploadImage) async {
+    final request = http.MultipartRequest("POST", Uri.parse(uri));
+    final headers = {"Content-type": "multipart/form-data"};
+
+    request.files.add(http.MultipartFile(
+        'audio',
+        uploadImage.readAsBytes().asStream(),
+        uploadImage.lengthSync(),
+        filename: uploadImage.path.split("/").last
+    ));
+
+    request.headers.addAll(headers);
+    final response = await request.send();
+    http.Response res = await http.Response.fromStream(response);
+    final resJson = jsonDecode(res.body);
+    print("Response => $resJson");
+    setState(() {
+      responseList = resJson;
+      isLoading = false;
+      isPredictionFinish = true;
+    });
+    print("prediction => ${responseList["prediction"]}");
+  }
+  String handlePredct(String flappyBirdsec, String snakeGamesec, String brikeBrakerSec) {
+    setState(() {
+      status = "";
+      isLoading = true;
+    });
+    Future.delayed(Duration(seconds: classes), () {
+      setState(() {
+        isLoading = false;
+      });
+      return "Normal";
+    });
+    return "Normal";
+  }
+
 }
